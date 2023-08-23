35af53352e6be754b45e94df8691924dd5786de5
wrserne <wrserne@gmail.com>
2023-08-23 11:15:01 -0400

    all

--- Homepage.js
+++ Homepage.js
@@ -6,9 +6,6 @@ const app = express();
 const session = require('express-session');
 const dotenv = require('dotenv');
 const url = require('url');
-const jawsDBUrl = 'mysql://imy4l8lvoatg1p1u:n9nglukxz6j96ypr@jtb9ia3h1pgevwb1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/nf9fk46l4rajefsl';
-
-const parsedUrl = url.parse(jawsDBUrl);
 
 dotenv.config(); // Load environment variables from .env file
 app.use(session({
@@ -27,19 +24,24 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
+const jawsDBUrl = 'mysql://imy4l8lvoatg1p1u:n9nglukxz6j96ypr@jtb9ia3h1pgevwb1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/nf9fk46l4rajefsl';
+
+const parsedUrl = url.parse(jawsDBUrl);
+
+// Extract database connection details from the JawsDB URL
 const dbUser = parsedUrl.auth.split(':')[0];
 const dbPassword = parsedUrl.auth.split(':')[1];
 const dbHost = parsedUrl.host;
 const dbName = parsedUrl.pathname.split('/')[1];
 const dbPort = parsedUrl.port;
 
-// Now, you can use these extracted details in your MySQL connection configuration
+// Configure MySQL Database
 const connection = mysql.createConnection({
     host: dbHost,
     user: dbUser,
     password: dbPassword,
     database: dbName,
-    port: dbPort || 3306, // Use 3306 as the default MySQL port
+    port: dbPort || 3306 // Use 3306 as the default MySQL port
 });
 
 // Connect to MySQL
